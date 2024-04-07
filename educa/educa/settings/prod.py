766f203ebb9d42d3c8ae108c9735f9aa010ec06a
ubuntu-pc <akhayev.a@protonmail.com>
2024-04-07 16:14:52 +0500

    Added Dockerfile docker-compose

---
+++ educa/educa/settings/prod.py
@@ -0,0 +1,22 @@
+import os
+from .base import *  # noqa:F403
+
+DEBUG = False
+
+# fmt:off
+ADMIN = [
+    ('Akhat A', 'email@email.com'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
